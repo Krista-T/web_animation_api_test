3477513f2460942c49dd37c85fcc28732cc31f3d
krista <krista.tomicic@gmail.com>
2021-03-15 22:46:56 +0100

    positioning fixed

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 "use strict";
+
 const robotRight = document.querySelector("#robotRight");
-const robotLeft = document.querySelector(".left");
+const robotLeft = document.querySelector("#robotLeft");
 
 //animation
 const robotAnimate = {
-  duration: 5000,
-  iterations: 2,
+  duration: 4500,
+  iterations: 1,
   fill: "forwards",
   easing: "ease-in-out",
   direction: "alternate",
 };
 
+const robotAnimateReverse = {
+  delay: 800,
+  duration: 4500,
+  iterations: 1,
+  fill: "forwards",
+  easing: "ease-in-out",
+  direction: "reverse",
+};
+
 //keyframes
 const keyFramesRight = [
-  { transform: "translateX(-20vw)" },
-  { transform: "translateX(34vw)" },
+  { transform: "translateX(-50vw)" },
+  { transform: "translateX(0vw)" },
 ];
 
 const keyFramesLeft = [
-  { transform: "translateX(100vw)" },
-  { transform: "translateX(49vw)" },
+  { transform: "translateX(50vw)" },
+  { transform: "translateX(0vw)" },
 ];
 
 document.querySelector(".btn button").addEventListener("click", startAnimation);
 
 function startAnimation() {
+  robotRight.classList.remove("hide");
+  robotLeft.classList.remove("hide");
+
   const rightAnimation = robotRight.animate(keyFramesRight, robotAnimate);
   const leftAnimation = robotLeft.animate(keyFramesLeft, robotAnimate);
-  Animation.cancel(rightAnimation, leftAnimation);
+  //pause and start reverse
+  rightAnimation.onfinish = () => {
+    startReverse();
+  };
+  leftAnimation.onfinish = () => {
+    startReverse();
+  };
+}
+
+function startReverse() {
+  robotRight.animate(keyFramesRight, robotAnimateReverse);
+  robotLeft.animate(keyFramesLeft, robotAnimateReverse);
 }
