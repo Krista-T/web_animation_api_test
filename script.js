0b99d727f89ff961c2586be8a8fbe501d5b9cd2b
krista <krista.tomicic@gmail.com>
2021-03-16 09:43:41 +0100

    hand svg  added

--- script.js
+++ script.js
@@ -2,7 +2,36 @@
 
 const robotRight = document.querySelector("#robotRight");
 const robotLeft = document.querySelector("#robotLeft");
+const btn = document.querySelector("#btn button");
+//FETCH AND ADD CSS ANIMATION
+window.addEventListener("DOMContentLoaded", loadSVG);
 
+function loadSVG() {
+  console.log("load the SVG");
+
+  fetch("hand.svg")
+    .then((response) => response.text())
+    .then((svgData) => {
+      console.log("svgData");
+
+      // put the SVG into the DOM
+      document.querySelector("#hand").innerHTML = svgData;
+
+      // TODO: Start the animation
+      runAnimation();
+    });
+}
+
+let car = null;
+let curve = null;
+let currentPosition;
+
+function runAnimation() {
+  console.log("animate");
+  document.querySelector("#hand").classList.add("pathLine");
+}
+
+//WEB API ANIMATION
 //animation
 const robotAnimate = {
   duration: 4500,
@@ -21,6 +50,13 @@ const robotAnimateReverse = {
   direction: "reverse",
 };
 
+// const btnAnimation = {
+//   duration: 1000,
+//   iterations: 1,
+//   fill: "forwards",
+//   easing: "ease-in-out",
+// }
+
 //keyframes
 const keyFramesRight = [
   { transform: "translateX(-50vw)" },
@@ -31,6 +67,12 @@ const keyFramesLeft = [
   { transform: "translateX(50vw)" },
   { transform: "translateX(0vw)" },
 ];
+
+// const keyFramesBtn = [
+//   { transform: "translateY(30vw)" },
+//   { transform: "translateY(0vw)" },
+//   {opacity: 80%},
+// ];
 
 document.querySelector(".btn button").addEventListener("click", startAnimation);
 
